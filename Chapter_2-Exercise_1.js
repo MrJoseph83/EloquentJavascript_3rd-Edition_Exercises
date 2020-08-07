c3cde7251928ac2f810b3a556df236632fbe107f
Joseph <josephmrobinsonsr@gmail.com>
2020-08-07 12:50:32 -0400

    2_1

--- Chapter_2-Exercise_1.js
+++ Chapter_2-Exercise_1.js
@@ -1,21 +1,4 @@
-//Looping a triangle
-/*
-Write a loop that makes seven calls to console.log to output the following triangle:
 
-#
-##
-###
-####
-#####
-######
-#######
-
-It may be useful to know that you can find the length of a string by writing .length after it.
-
-let abc = "abc";
-console.log(abc.length);
-// → 3
-*/
 
 for (let number = "#"; number.length < 8; number += "#") {
   console.log(number);
