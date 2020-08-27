a7313a0e15bf3d114d6e3b3df3fd6b8e199b9675
Joseph <josephmrobinsonsr@gmail.com>
2020-08-26 22:36:54 -0400

    4_2

--- Chapter_4-Exercise_2.js
+++ Chapter_4-Exercise_2.js
@@ -18,8 +18,6 @@ return array;
 }
 
 console.log(reverseArray(["A", "B", "C"]));
-// → ["C", "B", "A"];
 let arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
-// → [5, 4, 3, 2, 1]
