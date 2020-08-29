d29deef7270a73fddf476a668acadb8a270a4fe3
Joseph <josephmrobinsonsr@gmail.com>
2020-08-28 23:42:02 -0400

    9_2

--- Chapter_9-Exercise_2.js
+++ Chapter_9-Exercise_2.js
@@ -1,4 +1,3 @@
 let text = "'I'm the cook,' he said, 'it's my job.'";
 
-console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2'));
-// → "I'm the cook," he said, "it's my job."
+console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2'));// → "I'm the cook," he said, "it's my job."
